7118c3c7951446b6560c3ca41b90fe3026629f5e
Danny Spina <danny@ostec.de>
2021-01-06 21:20:48 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://inmammaveritas.netlify.com',
+  pathPrefix: '/',
+  title: 'In Mamma Veritas',
+  subtitle: 'Allattamento, come nessuno ve lo aveva mai raccontato.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'In Mamma Veritas',
+    photo: '/photo.png',
+    bio: 'Allattamento, come nessuno ve lo aveva mai raccontato.',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
